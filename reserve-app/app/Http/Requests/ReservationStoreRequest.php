ce982e3adf7f662cae890d22e7349e06e77701c6
Samiha <samihatasfiazaman@gmail.com>
2024-05-29 08:48:19 +0600

    edit

--- reserve-app/app/Http/Requests/ReservationStoreRequest.php
+++ reserve-app/app/Http/Requests/ReservationStoreRequest.php
@@ -8,11 +8,7 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class ReservationStoreRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
+   
     public function authorize()
     {
         return true;
